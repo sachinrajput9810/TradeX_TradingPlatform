8e68d70cb995ee64fd2e4832b2cd43710c142eed
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-06-01 13:42:34 +0530

    Watchlist page added

--- src/Page/Watchlist/Watchlist.jsx
+++ src/Page/Watchlist/Watchlist.jsx
@@ -8,20 +8,29 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
+import { BookmarkFilledIcon } from '@radix-ui/react-icons'
 const Watchlist = () => {
+
+  const handleRemoveToWatchlist = (value) => {
+    console.log("Remove from watchlist", value);
+  }
+
+
   return (
 <div className='p-5 lg:px-20'>
        <h1 className='font-bold text-3xl pb-5'>Watchlist</h1>
-      <Table>
+      <Table className="border">
       <TableHeader>
         <TableRow>
-          <TableHead className="">Asset</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Unit</TableHead>
-          <TableHead>Change</TableHead>
-          <TableHead>Change%</TableHead>
-          <TableHead className="text-right">Volume</TableHead>
+          <TableHead className="py-5">Coin</TableHead>
+          <TableHead>SYMBOL</TableHead>
+          <TableHead>VOLUME</TableHead>
+          <TableHead>MARKET CAP</TableHead>
+          <TableHead>24h</TableHead>
+          <TableHead className="">PRICE</TableHead>
+          <TableHead className="text-right text-red-600">REMOVE</TableHead>
         </TableRow>
       </TableHeader>
 
@@ -42,7 +51,18 @@ const Watchlist = () => {
           <TableCell>36,316,873,480</TableCell>
           <TableCell>2,062,249,389,058</TableCell>
           <TableCell>-1.49%</TableCell>
-          <TableCell className="text-right">$103,771</TableCell>
+          <TableCell className="">$103,771</TableCell>
+          <TableCell className="text-right">  
+              <Button
+                    className="h-10 w-10 bg-transparent text-white hover:bg-transparent focus:bg-transparent active:bg-transparent"
+                    onClick={() => handleRemoveToWatchlist(item.id)}
+                    size="icon"
+                    variant="ghost"
+                  >
+                    <BookmarkFilledIcon className="text-white w-6 h-6" />
+              </Button>
+
+          </TableCell>
         </TableRow> )  }
        
       </TableBody>
