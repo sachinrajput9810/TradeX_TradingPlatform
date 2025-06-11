e0eb8df6dde55aee896ede3a2d54d40272b3ff12
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-06-11 16:33:01 +0530

    Updated the BASE_URL to request on AWS server in api.js

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080"
+// export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "http://tradexapp-env.eba-p3jjfcsu.us-east-1.elasticbeanstalk.com";
+
+
 
  const api = axios.create({
     baseURL : API_BASE_URL,
