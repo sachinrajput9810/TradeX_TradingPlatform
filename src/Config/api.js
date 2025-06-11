0326ed1db4f79b199727ea8cdfab1d9cad88399a
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-06-11 16:48:38 +0530

    Modified api.js for Base url

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080"
-// export const API_BASE_URL = "http://tradexapp-env.eba-p3jjfcsu.us-east-1.elasticbeanstalk.com";
+// export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "http://tradexapp-env.eba-p3jjfcsu.us-east-1.elasticbeanstalk.com";
 
 
 
